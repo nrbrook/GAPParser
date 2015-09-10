5d484f01c0d4bc182039c1bcd058623bd6bc7723
Nick Brook <nrbrook@gmail.com>
2015-09-10 18:21:07 +0100

    Remove HTML from gap parser

--- GAPParser.js
+++ GAPParser.js
@@ -218,7 +218,7 @@
             return this.data.length > 0;
         },
         typeDescription: function() {
-            return (ret.GAPField.typeToStringMap[this.type] || 'Unknown') + (this.truncatedBy > 0 ? ' <span style="color:red;">(invalid length, missing '+this.truncatedBy+' bytes)</span>' : '');
+            return ret.GAPField.typeToStringMap[this.type] || 'Unknown';
         },
         dataDescription: function() {
             if(!this.validate()) {
